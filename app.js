59a2c6eb32ad2d587de0af1ab07e052c448fe2bf
Jonathan Alvaro <yokanaro@gmail.com>
2018-11-12 04:15:17 +0700

    t

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ function handleEvent(event) {
   };
 
   // ================ TASK INPUT AND NOTIFICATION ==================== //
+  console.log(message);
+  console.log(typeof(message));
   if(message === "1" || message === "2" || message === "3") {
     replymessage = course.showClassDetail(message);
   }
