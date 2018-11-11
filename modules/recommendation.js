efd5fc986bdf1800b35895cf243924eb8c2ac4a3
Jonathan Alvaro <yokanaro@gmail.com>
2018-11-12 01:42:45 +0700

    t

--- modules/recommendation.js
+++ modules/recommendation.js
@@ -93,19 +93,9 @@ var recommendClasses = function() {
               },
               "actions": [
                   {
-                      "type": "postback",
-                      "label": "Buy",
-                      "data": "action=buy&itemid=111"
-                  },
-                  {
-                      "type": "postback",
-                      "label": "Add to cart",
-                      "data": "action=add&itemid=111"
-                  },
-                  {
-                      "type": "uri",
+                      "type": "message",
                       "label": "View detail",
-                      "uri": "http://example.com/page/111"
+                      "text": "Show details for class Machine Learning"
                   }
               ]
             },
