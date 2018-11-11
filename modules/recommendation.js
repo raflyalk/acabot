753369fb0e18c95fe1401ee0104aa299b471308b
Jonathan Alvaro <yokanaro@gmail.com>
2018-11-12 01:22:33 +0700

    t

--- modules/recommendation.js
+++ modules/recommendation.js
@@ -39,6 +39,8 @@ var recommendClasses = function() {
     var columns = [];
 
     for(var i = 0; i < columnNumber; i++) {
+        console.log(classDescriptions[classNames[i]]);
+        console.log(classNames[i]);
         var column = {
             "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
             "imageBackgroundColor": "#FFFFFF",
